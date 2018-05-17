d3c2247055e6ac5ac9cde7b5ca1e8ce7d76ed1d4
Thom P <tpalstra@gmail.com>
2018-05-17 17:08:14 +0200

    CHG

--- common/web/js/app/app.core.js
+++ common/web/js/app/app.core.js
@@ -10,6 +10,8 @@ let define = function( name, object, aliases ){
   } catch (err) { console.warn( err ); }
 }
 
+define( "doc", document );
+
 define( "app", function( querySelector ){
   return document.querySelectorAll( querySelector );
 }, [ "$", "$$", "_", "test" ] );
@@ -19,14 +21,7 @@ app.extend = function( list ){
     list: list,
     with: function( ext, forceProperty ){
       for( let i = 0; i < this.list.length; i++){
-        var target;
-        if( ( typeof this.list[i].prototype !== "undefined" && forceProperty != true ) ){
-          target = this.list[i].prototype;
-          console.log( this.list[i], "is a prototype" );
-        } else {
-          target = this.list[i];
-          console.log( this.list[i], "is an object" );
-        }
+        var target = ( typeof this.list[i].prototype !== "undefined" && forceProperty != true ) ? this.list[i].prototype : this.list[i];
 
         for( let i in ext ){
           target[ i ] = ext[i];
@@ -53,8 +48,85 @@ extend( app, derp ).with( {
 extend( Node ).with( {
   on: function( eventTypes, b, c, d ){
     eventTypes.split( " " ).forEach( ( eventType ) => {
-
+      if( typeof b == "function" ){
+        this.addEventListener( eventType, b );
+      } else if( typeof b == "string" && typeof c == "function" ){
+        document.addEventListener( eventType, function( originalEvent ) {
+          if( originalEvent.target.matches( b ) ){
+            c.call( originalEvent.target, originalEvent )
+          } else if( closest = originalEvent.target.closest( b ) ){
+            c.call( closest, originalEvent );
+          }
+        } );
+      }
     } );
+  },
+  slideUp: function( speedInMs ){
+    if( this.slideTimeout ){
+      clearTimeout( this.slideTimeout );
+    }
+
+    var computed = window.getComputedStyle( this );
+    var currentHeight = parseInt( computed["height"] );
+
+    if( Number.isNaN( currentHeight ) ){ currentHeight = "0"; }
+
+    this.style["height"] = currentHeight;
+    this.style["overflow"] = "hidden";
+    this.style["transition"] = speedInMs + "ms linear";
+    this.style["height"] = "0";
+
+    this.slideTimeout = setTimeout( function ( event ) {
+
+      clearTimeout( this.slideTimeout );
+
+      this.style["display"] = "none";
+      this.style["height"] = "";
+      this.style["transition"] = "";
+      this.style["overflow"] = "";
+
+    }.bind(this), speedInMs );
+  },
+  slideDown: function( speedInMs ){
+    if( this.slideTimeout ){
+      clearTimeout( this.slideTimeout );
+    }
+
+    var computed = window.getComputedStyle( this );
+    var currentHeight = parseInt( computed["height"] );
+
+    if( Number.isNaN( currentHeight ) ){ currentHeight = "0"; }
+
+    this.style["display"] = "";
+    this.style["height"] = "";
+    this.style["transition"] = "";
+    this.style["overflow"] = "";
+
+    var realHeight = parseInt( computed["height"] );
+
+    this.style["height"] = currentHeight;
+    this.style["transition"] = speedInMs + "ms linear";
+    this.style["overflow"] = "hidden";
+
+    this.slideTimeout = setTimeout( function ( event ) {
+      clearTimeout( this.slideTimeout );
+      this.style["height"] = realHeight + "px";
+    }.bind(this), 10 );
+  },
+  slideToggle: function( speedInMs ){
+    if( parseInt( window.getComputedStyle( this )["height"] ) > 0 ){
+      this.slideUp( speedInMs );
+    } else {
+      this.slideDown( speedInMs );
+    }
+  }
+} );
+extend( Document, Node ).with( {
+  one: function( querySelector ){
+    return this.querySelector( querySelector );
+  },
+  find: function( querySelector ){
+    return this.querySelectorAll( querySelector );
   }
 } );
 extend( NodeList, HTMLCollection ).with( {
--- env/content/views/std/index.php
+++ env/content/views/std/index.php
@@ -1,1 +1,24 @@
-this is my index
+haha
+hoho
+<h2 id="toggler">Click me</h2>
+<p id="toggler-content" style="display: none;">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed velit quis nibh interdum euismod quis et turpis. Ut ac urna mattis, sagittis justo eget, vulputate enim. In ut nulla a enim finibus consequat id sed neque. Morbi tincidunt, tellus sit amet suscipit porttitor, ligula tortor pretium odio, pellentesque vestibulum justo dolor vel est. Quisque laoreet nisi in mi porta, nec dignissim quam bibendum. Sed dictum ex vitae viverra venenatis. Sed nec urna leo. Sed vestibulum nisl non magna fringilla, id blandit nibh pretium. In nec commodo neque. Etiam vulputate tellus sit amet rutrum mattis. Nam congue lectus quis quam ornare fermentum. Quisque sagittis a ante a mattis. Cras facilisis ipsum ut augue scelerisque, eget vehicula ipsum volutpat.
+
+  Suspendisse potenti. Nam sit amet est tortor. Aenean nec felis sed quam volutpat rhoncus vitae vel dolor. Vestibulum sodales condimentum egestas. Duis vulputate sodales ipsum, et aliquet ligula tristique ac. Fusce dictum lobortis hendrerit. Ut eu elit elementum, elementum quam in, finibus velit. Aliquam erat volutpat. Nam lacinia ex sit amet odio finibus, vel malesuada massa rutrum. Etiam tempor arcu vel sapien interdum iaculis.
+
+  Mauris a turpis interdum, laoreet massa id, accumsan quam. Vestibulum in dictum diam. Nunc pharetra tortor at enim condimentum aliquet. Aliquam semper vehicula leo sit amet feugiat. Curabitur luctus pretium felis et aliquam. Curabitur condimentum, arcu sed mollis feugiat, sapien mauris eleifend leo, vitae maximus justo tortor quis augue. In vel vestibulum quam, non auctor ipsum. In gravida placerat odio, et mollis neque pellentesque ac. Aliquam sit amet nisl et tellus vehicula fermentum. Phasellus eget gravida enim.
+
+  Curabitur sit amet lorem aliquam, posuere orci porta, ullamcorper metus. Pellentesque malesuada dapibus nibh a convallis. Mauris eget felis ullamcorper, sodales elit sed, mollis ligula. Nunc quis orci mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique neque vitae libero varius feugiat. Phasellus non mi lorem. Integer non diam lobortis, suscipit lacus sed, viverra metus. Phasellus interdum massa purus, eu eleifend lorem mattis vitae. Suspendisse fringilla eros et ante cursus, nec commodo velit suscipit. Integer mollis, metus cursus lobortis faucibus, nisl erat egestas turpis, vel faucibus velit elit at magna. Nulla enim lectus, lacinia semper congue eleifend, porta sed nisl.
+
+  Phasellus in sapien nisl. Praesent sagittis scelerisque nisi, et ultrices magna suscipit nec. Ut suscipit molestie est id fermentum. Fusce iaculis non velit in consequat. Nam eget purus massa. In mattis enim nec augue auctor, id vestibulum ex feugiat. Nullam rutrum dui tortor, a dictum lorem molestie ut. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis pellentesque faucibus leo, vitae porta dui fringilla quis. Aliquam erat volutpat. Vestibulum tortor purus, sagittis id magna eu, euismod pulvinar ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi scelerisque ex eu ante auctor, eget mollis libero imperdiet.
+</p>
+
+
+<script>
+document.addEventListener( "DOMContentLoaded", function( event ) {
+  doc.one("#toggler").on( "click", function( event ) {
+    doc.one("#toggler-content").slideToggle( 2000 );
+  } );
+} );
+
+</script>
